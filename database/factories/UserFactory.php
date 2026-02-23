3329ef6b549957725ef59245f5936a3caefd36d7
Suprise Nkosi <suprise.nkosi@example.com>
2026-02-23 19:26:07 +0200

    fix: use faker property in UserFactory to resolve seeder error

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'dietician_number' => fake()->unique()->numerify('DTN#####'),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'dietician_number' => $this->faker->unique()->numerify('DTN#####'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
